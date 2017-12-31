a4cc1b458c7739ee6853329aa3dc793f41ce6209
fredakilla <fredakilla@gmail.com>
2017-12-31 15:47:19 +0100

    bgfx

--- samples/Test/src/TestSample.cpp
+++ samples/Test/src/TestSample.cpp
@@ -3,7 +3,7 @@
 // Declare our game instance
 TestSample game;
 
-
+#if 1
 /**
  * Creates a triangle mesh with vertex colors.
  */
@@ -68,7 +68,7 @@ static Mesh* createTriangleMesh()
 
 
 
-    int indices[] =
+    short indices[] =
     {
         0,1,2,
         2,3,0
@@ -80,6 +80,63 @@ static Mesh* createTriangleMesh()
 
     return mesh;
 }
+#endif
+
+/*
+static Mesh* createTriangleMesh(float size = 1.0f)
+{
+    float a = size * 0.5f;
+    float vertices[] =
+    {
+        -a, -a,  a,    0.0,  0.0,  1.0,   0.0, 0.0,
+         a, -a,  a,    0.0,  0.0,  1.0,   1.0, 0.0,
+        -a,  a,  a,    0.0,  0.0,  1.0,   0.0, 1.0,
+         a,  a,  a,    0.0,  0.0,  1.0,   1.0, 1.0,
+        -a,  a,  a,    0.0,  1.0,  0.0,   0.0, 0.0,
+         a,  a,  a,    0.0,  1.0,  0.0,   1.0, 0.0,
+        -a,  a, -a,    0.0,  1.0,  0.0,   0.0, 1.0,
+         a,  a, -a,    0.0,  1.0,  0.0,   1.0, 1.0,
+        -a,  a, -a,    0.0,  0.0, -1.0,   0.0, 0.0,
+         a,  a, -a,    0.0,  0.0, -1.0,   1.0, 0.0,
+        -a, -a, -a,    0.0,  0.0, -1.0,   0.0, 1.0,
+         a, -a, -a,    0.0,  0.0, -1.0,   1.0, 1.0,
+        -a, -a, -a,    0.0, -1.0,  0.0,   0.0, 0.0,
+         a, -a, -a,    0.0, -1.0,  0.0,   1.0, 0.0,
+        -a, -a,  a,    0.0, -1.0,  0.0,   0.0, 1.0,
+         a, -a,  a,    0.0, -1.0,  0.0,   1.0, 1.0,
+         a, -a,  a,    1.0,  0.0,  0.0,   0.0, 0.0,
+         a, -a, -a,    1.0,  0.0,  0.0,   1.0, 0.0,
+         a,  a,  a,    1.0,  0.0,  0.0,   0.0, 1.0,
+         a,  a, -a,    1.0,  0.0,  0.0,   1.0, 1.0,
+        -a, -a, -a,   -1.0,  0.0,  0.0,   0.0, 0.0,
+        -a, -a,  a,   -1.0,  0.0,  0.0,   1.0, 0.0,
+        -a,  a, -a,   -1.0,  0.0,  0.0,   0.0, 1.0,
+        -a,  a,  a,   -1.0,  0.0,  0.0,   1.0, 1.0
+    };
+    short indices[] =
+    {
+        0, 1, 2, 2, 1, 3, 4, 5, 6, 6, 5, 7, 8, 9, 10, 10, 9, 11, 12, 13, 14, 14, 13, 15, 16, 17, 18, 18, 17, 19, 20, 21, 22, 22, 21, 23
+    };
+    unsigned int vertexCount = 24;
+    unsigned int indexCount = 36;
+    VertexFormat::Element elements[] =
+    {
+        VertexFormat::Element(VertexFormat::POSITION, 3),
+        VertexFormat::Element(VertexFormat::COLOR, 4),
+        VertexFormat::Element(VertexFormat::TEXCOORD0, 2)
+    };
+    Mesh* mesh = Mesh::createMesh(VertexFormat(elements, 3), vertexCount, false);
+    if (mesh == NULL)
+    {
+        GP_ERROR("Failed to create mesh.");
+        return NULL;
+    }
+    mesh->setVertexData(vertices, 0, vertexCount);
+    MeshPart* meshPart = mesh->addPart(Mesh::TRIANGLES, Mesh::INDEX16, indexCount, false);
+    meshPart->setIndexData(indices, 0, indexCount);
+    return mesh;
+}
+*/
 
 
 TestSample::TestSample()
